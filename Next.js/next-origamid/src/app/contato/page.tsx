cd95b110c34f0f03339b629d5e05ee03baef73be
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2025-07-03 18:06:22 -0300

    feat: 0201 App Router

---
+++ Next.js/next-origamid/src/app/contato/page.tsx
@@ -0,0 +1,8 @@
+export default function ContatoPage() {
+  return (
+    <main>
+      <h1>Contato</h1>
+      <p>Essa é a página Contato</p>
+    </main>
+  )
+}
---
+++ Next.js/next-origamid/src/app/sobre/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Sobre',
+  description: 'Essa é a página Sobre do projeto Next.js da Origamid',
+}
+
+// Para o sistema de rotas do Next.js, a página Sobre deve ser criada dentro da pasta `app/sobre`, e o arquivo deve ser nomeado como `page.tsx` ou `page.jsx` e ser exportado como um componente React padrão. O nome da função não importa, mas iremos usar um padrão de nomenclatura para facilitar a identificação. Nome da rota(nome da pasta) + "Page". Ex: SobrePage
+export default function SobrePage() {
+  return (
+    <main>
+      <h1>Sobre</h1>
+      <p>Essa é a página Sobre</p>
+    </main>
+  )
+}
