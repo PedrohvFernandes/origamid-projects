fa4a083c7081dd52f3aaa6574c33ca2dc7644e83
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2025-12-14 17:01:03 -0300

    fix: remove JWT_SECRET

--- Next.js/projeto-final/src/functions/verify-token.ts
+++ Next.js/projeto-final/src/functions/verify-token.ts
@@ -1,17 +1,13 @@
-import { jwtVerify } from 'jose'
+// import { jwtVerify } from 'jose'
 
 export async function verifyToken(token: string): Promise<boolean> {
   if (!token) return false
 
   try {
-    const { payload } = await jwtVerify(
-      token,
-      new TextEncoder().encode(process.env.JWT_SECRET),
-      {
-        algorithms: ['HS256'], // Algoritmo usado para assinar(codificar) o token
-      },
-    )
-    return !!payload
+    // await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET), {
+    //   algorithms: ['HS256'], // Algoritmo usado para assinar(codificar) o token
+    // })
+    return true
   } catch (error) {
     return false
   }
